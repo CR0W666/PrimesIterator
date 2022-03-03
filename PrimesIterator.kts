2703577b3c8ad0e23504b67929cb1e8f6e0fc02d
Zen1th <lukas.petracek@educanet.cz>
2022-03-03 19:31:46 +0100

    light update

--- PrimesIterator.kts
+++ PrimesIterator.kts
@@ -1,20 +1,23 @@
 fun main() {
     val primes = Iterable { PrimesIterator(10) }
+    primes
     for (prime in primes) println(prime)
 }
 main()
 
 
-class PrimesIterator(private val ceil: Int, var calcMode: mode = mode.AMOUNT) : Iterator<Int> {
-    enum class mode { AMOUNT, TO }
+class PrimesIterator(private val ceil: Int, private var calcMode: Mode = Mode.AMOUNT) : Iterator<Int> {
+    enum class Mode { AMOUNT, TO }
     private var counter: Int = 0
     private val foundPrimes: MutableList<Int> = mutableListOf<Int>()
 
-    override fun hasNext(): Boolean = if ( calcMode == mode.AMOUNT ) foundPrimes.size < ceil else counter == ceil
+    fun changeMode(newMode: Mode) { calcMode = newMode }
+
+    override fun hasNext(): Boolean = if ( calcMode == Mode.AMOUNT ) foundPrimes.size < ceil else counter == ceil
 
     override fun next(): Int {
         check( hasNext() )
-
+        println(calcMode)
         //TODO
 
         foundPrimes.add(0)
